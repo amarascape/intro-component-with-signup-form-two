92fc97865df474b516dc98485bbee21d4c015384
amarascape <amara.brun.01@gmail.com>
2021-08-11 13:34:18 -0400

    JSX tags added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,44 @@ import "./App.css";
 // || ELEMENT BUILDERS
 
 function Main() {
+  function formPreventDefault(e) {
+    alert("formPreventDefault called, form will not submit");
+    e.preventDefault();
+  }
+
+  function onClickPreventDefaul(e) {
+    alert("onClickPreventDefaul called, form will not submit");
+    e.preventDefault();
+  }
+
   return (
     <>
-      Learn to code by watching others See how experienced developers solve
-      problems in real-time. Watching scripted tutorials is great, but
-      understanding how developers think is invaluable. Try it free 7 days then
-      $20/mo. thereafter First Name Last Name Email Address Password Claim your
-      free trial By clicking the button, you are agreeing to our Terms and
-      Services
+      <main>
+        <h1>Learn to code by watching others</h1>
+        <p id="leadingParagraph">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+        <div className="tryItFree">
+          Try it free 7 days then $20/mo. thereafter
+        </div>
+        <form id="signUpForm" onSubmit={(e) => formPreventDefault(e)}>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Password" />
+          <input
+            type="submit"
+            name="submit"
+            value="Claim your free trial"
+            onClick={(e) => onClickPreventDefaul(e)}
+          />
+          <p id="consentWarning">
+            By clicking the button, you are agreeing to our Terms and Services
+          </p>
+        </form>
+      </main>
     </>
   );
 }
@@ -20,12 +50,13 @@ function Footer() {
   return (
     <>
       <footer>
-        <p class="attribution">
+        <p className="attribution">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
             Frontend Mentor
           </a>
-          . Coded by <a href="#">amarascape</a>.
+          . Coded by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge">amarascape</a>.
         </p>
       </footer>
     </>
